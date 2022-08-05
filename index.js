f5db08525dfea528e9c6c5f731abba75edcec1bc
Sharonah8 <nyamongosharon88@gmail.com>
2022-08-05 07:40:53 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(n) {
+    let x = parseInt(n, 10);
+    return x;
+}
+
+function preserveDecimal(n) {
+    let y = parseFloat(n);
+    return y;
+}
